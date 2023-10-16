ebeb85f3bfae63f7e698e96e2b821cffb057322b
Guido Genzone <ggenzone@protonmail.com>
2023-10-16 13:46:30 +0200

    Add basic drawing capability

--- src/features/draw/index.tsx
+++ src/features/draw/index.tsx
@@ -1,8 +1,28 @@
 import { Separator } from '@/components/ui/separator'
-import { useRef } from 'react'
+import { useDraw } from '@/hooks/use-draw'
+import { useState } from 'react'
 
 export default function PlaygroundPage () {
-  const refCanvas = useRef<HTMLCanvasElement | null>(null)
+  const [color, setColor] = useState<string>('#ccc')
+  const [lineWidth, setLineWidth] = useState<number>(5)
+  const { canvasRef, onMouseDown } = useDraw({ onDraw })
+
+  function onDraw ({ prevPoint, ctx, currentPoint }: Draw) {
+    const startPoint = prevPoint ?? currentPoint
+
+    ctx.beginPath()
+    ctx.lineWidth = lineWidth
+    ctx.strokeStyle = color
+    ctx.moveTo(startPoint.x, startPoint.y)
+    ctx.lineTo(currentPoint.x, currentPoint.y)
+    ctx.stroke()
+
+    ctx.fillStyle = color
+    ctx.beginPath()
+    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI)
+    ctx.fill()
+  }
+
   return (
     <>
       <div className="h-full flex-col flex">
@@ -19,11 +39,23 @@ export default function PlaygroundPage () {
             <div className="grid h-full items-stretch gap-6 md:grid-cols-[1fr_200px]">
               <div className="hidden flex-col space-y-4 sm:flex md:order-2">
                 Widgets
+                <div className='mt-2 flex items-center justify-between'>
+                  <label className='block'> Color
+                    <input type='color' onChange={(ev) => { setColor(ev.target.value) }} value={color} />
+                  </label>
+                </div>
+                <div className='mt-2 flex items-center justify-between'>
+                  <label className='block'> Brush
+                    <input type='range' min={3} max={10} onChange={(ev) => { setLineWidth(Number(ev.target.value)) }} value={lineWidth} />
+                  </label>
+                </div>
               </div>
               <div className="md:order-1">
                 <div className="mt-0 border-0 p-0">
                   <div className="flex h-full flex-col space-y-4">
-                    <canvas ref={refCanvas} className='rounded-sm border'/>
+                    <div>
+                      <canvas onMouseDown={onMouseDown} ref={canvasRef} className='rounded-sm border border-black' width={750} height={750}/>
+                    </div>
                   </div>
                 </div>
               </div>
---
+++ src/hooks/use-draw.ts
@@ -0,0 +1,59 @@
+import { useEffect, useRef, useState } from 'react'
+
+interface UseDrawProps {
+  onDraw: ({ ctx, currentPoint, prevPoint }: Draw) => void 
+}
+
+export const useDraw = ({ onDraw }: UseDrawProps) => {
+  const [mouseDown, setMouseDown] = useState(false)
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const prevPoint = useRef<Point | null>(null)
+
+  const onMouseDown = () => { setMouseDown(true) }
+
+  useEffect(() => {
+    const onMouseUp = () => {
+      setMouseDown(false)
+      prevPoint.current = null
+    }
+
+    const handler = (e: MouseEvent) => {
+      if (!mouseDown) return
+      const currentPoint = computePointInCanvas(e)
+
+      const ctx = canvasRef.current?.getContext('2d')
+      if (ctx == null || currentPoint == null) {
+        return
+      }
+
+      onDraw({ctx, currentPoint, prevPoint: prevPoint.current })
+      prevPoint.current = currentPoint
+    }
+
+    const computePointInCanvas = (e:MouseEvent) => {
+      const canvas = canvasRef.current
+
+      if (canvas === null) {
+        return
+      }
+
+      const rect = canvas.getBoundingClientRect()
+      const x = e.clientX - rect.left
+      const y = e.clientY - rect.top
+
+      return { x, y }
+    }
+
+    if (canvasRef?.current !== null) {
+      canvasRef.current.addEventListener('mousemove', handler)
+    }
+    window.document.addEventListener('mouseup', onMouseUp)
+
+    return () => {
+      canvasRef?.current?.removeEventListener('mousemove', handler)
+      window.document.removeEventListener('mouseup', onMouseUp)
+    }
+  }, [onDraw])
+
+  return { canvasRef, onMouseDown }
+}
